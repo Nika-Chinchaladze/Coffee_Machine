b17584f9465361f9722d199424e395747eced8ec
Nika-Chinchaladze <106172218+Nika-Chinchaladze@users.noreply.github.com>
2022-10-10 23:33:45 +0400

    menu

---
+++ Available_menu.py
@@ -0,0 +1,23 @@
+class Espresso:
+    def __init__(self):
+        self.name = 'espresso'
+        self.water = 50
+        self.milk = 0
+        self.coffee = 20
+        self.cost = 1.5
+
+class Latte:
+    def __init__(self):
+        self.name = 'latte'
+        self.water = 200
+        self.milk = 150
+        self.coffee = 25
+        self.cost = 2.5
+
+class Cappuccino:
+    def __init__(self):
+        self.name = 'cappuccino'
+        self.water = 250
+        self.milk = 50
+        self.coffee = 30
+        self.cost = 3.5
